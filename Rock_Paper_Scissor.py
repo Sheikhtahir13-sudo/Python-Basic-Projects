af5b0e237d0842b3e0e8fa99d8725c641332a930
Muhammad Tahir <tahirff248@gmail.com>
2026-07-19 02:34:47 +0500

    Rock_Paper_Scissor_game.py
    This is also the one of the basic python program game that build logics of python basics .

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -11,30 +11,30 @@ while True:
         print("1 - Play game")
         print("2 - Quit\n")
     
-        choice = int(input("Enter choice(1-2): "))   #input method
+        choice = int(input("Enter choice(1-2): "))   # input method
     
         if choice == 1:
-            options = ['rock','paper','scissor']        #if-else condition
+            options = ['rock','paper','scissor']        # if-else condition
             com_score = 0   #score variables
             user_score = 0
         
-            com = random.choice(options)    #random module used
+            com = random.choice(options)    # random module used
         
             user = input("Enter choice (Rock,Paper & Scissor): ").lower()   #input method with lower case
         
-            if user not in options:      #for invalid choice 
+            if user not in options:      # For invalid choice 
                 print("Invalid option")
                 break
         
             elif user == com:
-                print("== CHOICES ==")           #game draw
+                print("== CHOICES ==")           # Game draw
                 print(f"Computer: {com}")
                 print(f"Player: {user}\n")
                 print("== Result ==")
                 print("Draw\n")
         
             elif (
-            (user == "rock" and com == "scissors") or     #when player win
+            (user == "rock" and com == "scissors") or     # When player win
             (user == "paper" and com == "rock") or
             (user == "scissors" and com == "paper")
             ):
@@ -43,7 +43,7 @@ while True:
             
         
             else:
-                print("== CHOICES ==")        #when computer wins
+                print("== CHOICES ==")        # When computer wins
                 print(f"Computer:{com}")
                 print(f"Player: {user}\n")
                 print("== Result ==")
@@ -51,10 +51,10 @@ while True:
                 com_score += 1
                 
         else:
-            print(f"Computer score: {com_score}")    #score display
+            print(f"Computer score: {com_score}")    # Score display
             print(f"Player score: {user_score}")
             print("Thanks for playing this game!")
             break
 
-    except Exception as e:
+    except Exception as e:                      # Error handling
         print(f"Error occured: {e}")
